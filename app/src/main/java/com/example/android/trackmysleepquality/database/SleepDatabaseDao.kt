a6fbfa89625f9b18be73e383c91c0b8762d1ff9d
raymondlam88 <48380674+raymondlam88@users.noreply.github.com>
2021-03-09 23:04:24 +0800

    Updated on the Dao with errors solved.

--- app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/database/SleepDatabaseDao.kt
@@ -38,6 +38,6 @@ interface SleepDatabaseDao{
     @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC LIMIT 1")
     fun getTonight():SleepNight?
 
-    @Query("SELECT * FROM daily_sleep_quality ORDER BY nightId DESC")
+    @Query("SELECT * FROM daily_sleep_quality_table ORDER BY nightId DESC")
     fun getAllNights():LiveData<List<SleepNight>>
 }
